83eb115400e09931bd70a3d5b4df198ce46f63e5
Sinali Dionou <sinaloid@gmail.com>
2022-07-30 11:44:57 +0000

    config liaison reactjs test 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,7 +102,7 @@
             }
         });
     </script>
-    <script src="{{asset('/js/app.js')}}" defer></script>
+    <script src="/spa-market/public/js/app.js" defer></script>
 
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
