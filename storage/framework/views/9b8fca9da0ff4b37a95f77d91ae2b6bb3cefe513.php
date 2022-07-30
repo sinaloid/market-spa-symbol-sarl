f78fb69d5d56d8a5a55f2d3b9de2ef3d7825ae3e
Sinali Dionou <sinaloid@gmail.com>
2022-07-30 11:03:28 +0000

    config liaison reactjs

--- storage/framework/views/9b8fca9da0ff4b37a95f77d91ae2b6bb3cefe513.php
+++ storage/framework/views/9b8fca9da0ff4b37a95f77d91ae2b6bb3cefe513.php
@@ -102,7 +102,7 @@
             }
         });
     </script>
-    <script src="/js/app.js" defer></script>
+    <script src="<?php echo e(asset('/js/app.js')); ?>" defer></script>
 
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
