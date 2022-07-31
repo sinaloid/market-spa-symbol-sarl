ec36fe2d8903add20d361e13a909e4da0faf26ee
Sinali Dionou <sinaloid@gmail.com>
2022-07-31 10:46:44 +0000

    config url react test 9

--- resources/js/app/services/api.js
+++ resources/js/app/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 //const URL = 'https://market-africadefis.herokuapp.com/api/'
-const URL = 'http://africadefis.com/spa-market/api/'
+const URL = 'http://africadefis.com/spa-market/public/api/'
 const URL2 = 'http://127.0.0.1:8000/api/'
 //const URLIMG = 'https://market-africadefis.herokuapp.com/uploads/'
-const URLIMG = 'http://africadefis.com/spa-market/uploads/'
+const URLIMG = 'http://africadefis.com/spa-market/public/uploads/'
 const URLIMG2 = 'http://127.0.0.1:8000/uploads/'
 
 export const urlImg = URLIMG
