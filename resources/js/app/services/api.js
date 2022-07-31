024d584e45b5cb8ba70b71a41c7274b03ad9f0e9
Sinali Dionou <sinaloid@gmail.com>
2022-07-31 12:32:22 +0000

    config url react ok

--- resources/js/app/services/api.js
+++ resources/js/app/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 //const URL = 'https://market-africadefis.herokuapp.com/api/'
-const URL = 'http://africadefis.com/spa-market/public/api/'
+const URL = 'http://market.africadefis.com/api/'
 const URL2 = 'http://127.0.0.1:8000/api/'
 //const URLIMG = 'https://market-africadefis.herokuapp.com/uploads/'
-const URLIMG = 'http://africadefis.com/spa-market/public/uploads/'
+const URLIMG = 'http://market.africadefis.com/uploads/'
 const URLIMG2 = 'http://127.0.0.1:8000/uploads/'
 
 export const urlImg = URLIMG
