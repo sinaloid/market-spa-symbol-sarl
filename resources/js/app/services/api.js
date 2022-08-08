f6213678f989c817b45279ab8807474ea4a7ea76
Sinali Dionou <sinaloid@gmail.com>
2022-08-08 12:20:09 +0000

    config url

--- resources/js/app/services/api.js
+++ resources/js/app/services/api.js
@@ -7,10 +7,10 @@ const URL2 = 'http://127.0.0.1:8000/api/'
 const URLIMG = 'http://market.africadefis.com/uploads/'
 const URLIMG2 = 'http://127.0.0.1:8000/uploads/'
 
-export const urlImg = URLIMG2
+export const urlImg = URLIMG
 
 const apiClient = axios.create({
-    baseURL: URL2,
+    baseURL: URL,
     withCredentials: true,
     headers: {'Accept':'application/json'},
 });
