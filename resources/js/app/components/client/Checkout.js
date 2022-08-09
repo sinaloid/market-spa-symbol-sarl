6262566e09c3301d24f1297307f5f10ecd0261ad
Sinali Dionou <sinaloid@gmail.com>
2022-08-09 20:39:28 +0000

    api redirection mobile ok

--- resources/js/app/components/client/Checkout.js
+++ resources/js/app/components/client/Checkout.js
@@ -106,35 +106,17 @@ const Checkout = () => {
         /*SendPaymentInfos(order_number,agency_code,secure_code,domain_name,url_redirection_suc
             cess,url_redirection_failed,amount,city,email,clientFirstName,clientLastName,)*/
     };
-    const detectDevice = () =>{
-        if(isMobile){
-            alert("Mobile")
+    const detectDevice = (type, pay) =>{
+        if(isMobile && pay){
+            handleOnClickShare()
         }else{
-            alert("Desktop")
+            handleSubmit(type, pay)
 
         }
     }
     const handleOnClickShare = () => {
-        if (navigator.share) {
-          navigator
-            .share({
-                url: `https://market.africadefis.com/paiement/app/${commandSlug}`,
-                text: "Ouvrez le lien dans un navigateur pour continuer le payment",
-                title: "Paiement AfricaDefis",
-            })
-            .then(() => {
-              console.log('Successfully shared');
-            })
-            .catch(error => {
-              console.error('Something went wrong sharing the blog', error);
-            });
-        }
-      };
-
-      window.mobileCheck = function() {
-        let check = false;
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
+        alert("ok");
+        window.open(`https://192.168.1.16:8000/paiement/app/${commandSlug}`, '_blank');
       };
     
     return (
@@ -183,13 +165,6 @@ const Checkout = () => {
                                         <button
                                             id="printInvoice"
                                             className="btn btn-afdefis ms-1 my-1"
-                                            onClick={()=>{
-                                                if(! window.mobileCheck){
-                                                    alert("mobile browser")
-                                                }else{
-                                                    alert("desktop browser")
-                                                }
-                                            }}
                                         >
                                             <i className="fa fa-print"></i>{" "}
                                             Imprimer
@@ -197,7 +172,7 @@ const Checkout = () => {
                                         <Link
                                             to="#"
                                             className="btn btn-afdefis ms-1 my-1"
-                                            onClick={()=>{detectDevice()}}
+                                            
                                         >
                                             <i className="fa fa-file-pdf-o"></i>{" "}
                                             Export PDF
@@ -500,40 +475,38 @@ const Checkout = () => {
                         </div>
                     </main>
 
-                    <div class="modal" id="myModal">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title">{
+                    <div className="modal" id="myModal">
+                        <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h4 className="modal-title">{
                                         (action.pay) ? "Paiement maintenant" : "Paiement prochainement"
                                     }</h4>
                                     <button
                                         type="button"
-                                        class="btn-close"
+                                        className="btn-close"
                                         data-bs-dismiss="modal"
                                     ></button>
                                 </div>
 
-                                <div class="modal-body">
+                                <div className="modal-body">
                                     {
                                         (action.pay) ? "Vous êtes sur le point de faire un paiement" : "Vous êtes sur le point de valider un paiement ultérieur"
                                     }
                                 </div>
 
-                                <div class="modal-footer">
+                                <div className="modal-footer">
                                     <button
                                         type="button"
-                                        class="btn btn-afdefis-secondary"
+                                        className="btn btn-afdefis-secondary"
                                         data-bs-dismiss="modal"
-                                        onClick={()=>{
-                                            handleSubmit(action.typeCommande, action.pay)
-                                        }}
+                                        onClick={()=>{detectDevice(action.typeCommande, action.pay)}}
                                     >
                                         Continuer
                                     </button>
                                     <button
                                         type="button"
-                                        class="btn btn-afdefis"
+                                        className="btn btn-afdefis"
                                         data-bs-dismiss="modal"
                                     >
                                         Annuler
